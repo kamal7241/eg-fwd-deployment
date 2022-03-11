0e68007166cc0066624c8e7e79cf9f0d09a245e6
kamalMohamed <kmohamed@turndigital.net>
2022-03-11 12:51:17 +0200

    backend deployment progress commit

---
+++ udagram-api/bin/deploy.sh
@@ -0,0 +1,3 @@
+cd www
+eb init --region $AWS_REGION $EP_APP
+eb deploy $EB_ENV
--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: config.port,
+export const sequelize = 
+new Sequelize('postgres://postgres:password123@database-1.cb1xleokdjpz.us-east-1.rds.amazonaws.com:5432/postgres') 
 
-  dialect: "postgres",
-  storage: ":memory:",
-});
+
+// new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   port: config.port,
+
+//   dialect: "postgres",
+//   storage: ":memory:",
+// });
+
