96c87d218271f28b288ae3e516d2695e7eadb90c
kamalMohamed <kmohamed@turndigital.net>
2022-03-12 23:01:44 +0200

    add enviroment variables to circleci instead of elasticbean stalk

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,2 +1,10 @@
 eb use Deployapp
+eb setenv AWS_REGION=$AWS_REGION
+eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv PORT=$PORT
+eb setenv PORTDB=$PORTDB
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb deploy Deployapp
